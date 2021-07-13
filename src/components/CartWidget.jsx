85d1bd0fdbebc8d509e8795b757670a036a88b3b
Sol Pichirili <sol.pichirili@gmail.com>
2021-07-13 20:34:01 -0300

    desafio 3

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {GiShoppingCart} from 'react-icons/gi';
 
 const CartWidget = () => {
     return (
         <div>
-            icono carrito
+            <GiShoppingCart className="icono"/>
         </div>
     )
 }
